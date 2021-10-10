abdc6bb2427e238ccc4f50ca49908e9456746cef
Piotr Morgwai Kotarbinski <foss@morgwai.pl>
2021-10-10 16:48:56 +0700

    license header

--- src/main/resources/reduce.c
+++ src/main/resources/reduce.c
@@ -1,3 +1,4 @@
+// Copyright (c) Piotr Morgwai Kotarbinski, Licensed under the Apache License, Version 2.0
 #pragma OPENCL EXTENSION cl_khr_fp64 : enable
 #pragma OPENCL EXTENSION cl_khr_subgroups : enable
 
